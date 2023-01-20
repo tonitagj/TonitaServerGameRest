0d9c429b91cabcf64b9db95c783ef5cd68f4a6d5
Tonita <tonitagjeluci@gmail.com>
2023-01-20 01:06:33 +0100

    This is the main commit for now.

--- src/main/java/com/coderfromscratch/httpserver/HttpServer.java
+++ src/main/java/com/coderfromscratch/httpserver/HttpServer.java
@@ -1,2 +1,51 @@
-package com.coderfromscratch.httpserver;public class HttpServer {
+package com.coderfromscratch.httpserver;
+
+import com.coderfromscratch.httpserver.config.Configuration;
+import com.coderfromscratch.httpserver.config.ConfigurationManager;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+/**
+ * Driver class for the http server
+ */
+public class HttpServer {
+    public static void main(String[] args){
+        System.out.println("Server starting...");
+
+        ConfigurationManager.getInstance().loadConfigurationFile("src/main/resources/http.json");
+        Configuration conf = ConfigurationManager.getInstance().getCurrentConfiguration();
+        System.out.println("Using Port: " + conf.getPort());
+        System.out.println("Using Webroot: " + conf.getWebroot());
+        System.out.println("Test!");
+        try {
+            ServerSocket serverSocket = new ServerSocket(conf.getPort());
+            Socket socket = serverSocket.accept();
+
+            InputStream inputStream = socket.getInputStream();
+            OutputStream outputStream = socket.getOutputStream();
+
+            String html = "<html><head><title>Simple JAVA HTTP Server</title> <body> <h1>Tonita Gjeluci</h1></body<> </head></html>";
+            final String CRLF="\n\r"; //13, 10
+
+            String response = "HTTP/1.1 200 OK" + CRLF +//Status Line :: Http version, respponse code, response message
+                    "Content-length:" + html.getBytes().length + CRLF + // Header
+                    CRLF +
+                    html +
+                    CRLF + CRLF;
+
+            outputStream.write(response.getBytes());
+
+            inputStream.close();
+            outputStream.close();
+            socket.close();
+            serverSocket.close();
+
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
 }
--- src/main/java/com/coderfromscratch/httpserver/config/ConfigurationManager.java
+++ src/main/java/com/coderfromscratch/httpserver/config/ConfigurationManager.java
@@ -1,2 +1,65 @@
-package com.coderfromscratch.httpserver.config;public class ConfigurationManager {
+package com.coderfromscratch.httpserver.config;
+
+import com.coderfromscratch.httpserver.util.Json;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonNode;
+
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class ConfigurationManager {
+    private static ConfigurationManager myConfigurationManager;
+    private static Configuration myCurrentConfiguration;
+    private ConfigurationManager() {
+    }
+
+    public static ConfigurationManager getInstance(){
+        if(myConfigurationManager==null)
+            myConfigurationManager=new ConfigurationManager();
+        return myConfigurationManager;
+    }
+
+    /**
+     * Used to load the Configuration file by the path provided
+     */
+    public void loadConfigurationFile(String filePath){
+        FileReader fileReader = null;
+        try {
+            fileReader = new FileReader(filePath);
+        } catch (FileNotFoundException e) {
+            throw new HttpConfigurationException(e);
+        }
+        StringBuffer sb = new StringBuffer();
+        int i;
+        try {
+            while ( ( i= fileReader.read()) != -1){
+                sb.append((char)i);
+            }
+        }catch (IOException e){
+            throw new HttpConfigurationException(e);
+        }
+        JsonNode conf = null;
+        try {
+            conf= Json.parse(sb.toString());
+        } catch (IOException e) {
+            throw new HttpConfigurationException("Error parsing the configuration file!", e);
+        }
+
+        try{
+            myCurrentConfiguration = Json.fromJson(conf, Configuration.class);
+        }catch (JsonProcessingException e){
+            throw new HttpConfigurationException("Error parsing configuration file, internal!", e);
+        }
+    }
+
+    /**
+     * Returns the current loaded configuration
+     */
+    public Configuration getCurrentConfiguration(){
+        if( myCurrentConfiguration == null){
+            throw new HttpConfigurationException("No Current Configuration!");
+        }
+        return myCurrentConfiguration;
+    }
 }
